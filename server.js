6dd7acee65d7054e38c9b4e2ba9e33df4fb8bc78
Ramon <ramon@startapp.one>
2021-06-12 19:51:04 -0300

    new logs

--- server.js
+++ server.js
@@ -15,16 +15,16 @@ function action(event){
   if(!retweeted_status && !is_quote_status){
     Tweet.post(`statuses/retweet/${id_str}`, erro => { 
       if(erro){
-        console.log("Erro no retweet: " + erro)
+        console.log("Houve um erro com o retweet: " + erro)
       }else {
-        console.log("RETWEETADO: ", `https://twitter.com/${name}/status/${id_str}`)
+        console.log("Retweet: ", `https://twitter.com/${name}/status/${id_str}`)
       }
     })
     Tweet.post('favorites/create', {id: id_str}, erro => {
       if(erro){
-        return console.log("Erro no like: " + erro) 
+        return console.log("Houve um erro com o like: " + erro) 
       }else {
-        return console.log("Tweet Likado. URL do Tweet: " + `https:twitter.com/${screen_name}/status/${id_str}`) 
+        return console.log("Tweet liked. URL: " + `https:twitter.com/${screen_name}/status/${id_str}`) 
       }
     }) 
   }else {
@@ -33,5 +33,5 @@ function action(event){
 }
 
 var stream = Tweet.stream("statuses/filter", {track: "BBMP"});
-stream.on('data', action);
-stream.on('error', erro => console.log("Erro: "+ erro));
+stream.on("data", action);
+stream.on("error", erro => console.log("Erro: "+ erro));
