ce594be0964f555141fe7eac8f89df9f87774205
Ramon <ramon@startapp.one>
2021-07-05 20:33:16 -0300

    feat: now reoly with "Bora Bahêa ?!"

--- server.js
+++ server.js
@@ -8,23 +8,26 @@ const Tweet = new Twitter({
   access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET,
 })
 
-function action(event) {
-  const { retweeted_status, id_str} = event;
+const action = (event) => {
+  const { retweeted_status, id_str } = event;
   const { name } = event.user;
 
+  const tweeted = (err) => {
+    if (err) {
+      console.log(err.message);
+    } else {
+      console.log("Olha só, me parece que algo deu certo...");
+    }
+  }
+
   if (!retweeted_status && name !== "CowinBangalore") {
-    Tweet.post("favorites/create", { id: id_str }, erro => {
-      if (erro) {
-        return console.log("Houve um erro com o like: " + erro)
-      } else {
-        console.log("Alguém recebeu um like")
-      }
-    })
+    Tweet.post("favorites/create", { id: id_str }, tweeted)
+    Tweet.post("statuses/update", { status: "Bora Bahêa ?!", in_reply_to_status_id: id_str }, tweeted);
   } else {
     return
   }
 }
 
-var stream = Tweet.stream("statuses/filter", { track: "BBMP"});
+var stream = Tweet.stream("statuses/filter", { track: "BBMP" });
 stream.on("data", action);
-stream.on("error", erro => console.log("Erro: " + erro));
+stream.on("error", erro => console.log("Erro barril: " + erro));
