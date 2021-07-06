333ee498f573078f0d0ef4b8a0c38605b2a762fc
Ramon <ramon@startapp.one>
2021-07-05 23:09:20 -0300

    fix reply

--- server.js
+++ server.js
@@ -6,24 +6,23 @@ const Tweet = new Twitter({
   consumer_secret: process.env.BOT_CONSUMER_KEY_SECRET,
   access_token_key: process.env.BOT_ACCESS_TOKEN,
   access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET,
-  timeout_ms: 60 * 1000,
 })
 
 const action = (event) => {
-  const { retweeted_status, id_str, in_reply_to_status_id, is_quote_status } = event;
-  const { name } = event.user;
+  const { retweeted_status, id_str, is_quote_status } = event;
+  const { screen_name } = event.user;
 
   const tweeted = (err) => {
     if (err) {
-      console.log(err.message);
+      console.log("Algo deu muito errado", err.message);
     } else {
       console.log("Olha só, me parece que algo deu certo...");
     }
   }
 
-  if (!retweeted_status && !is_quote_status && name !== "CowinBangalore") {
+  if (!retweeted_status && !is_quote_status && screen_name !== "CowinBangalore") {
     Tweet.post("favorites/create", { id: id_str }, tweeted)
-    Tweet.post("statuses/update", { in_reply_to_status_id: in_reply_to_status_id, status: "Bora Bahêa ?!"}, tweeted);
+    Tweet.post("statuses/update", { status: `@${screen_name} Bora Bahêa ?!`, in_reply_to_status_id: id_str}, tweeted);
   } else {
     return
   }
